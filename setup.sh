0f422a7d83367c1f17a4303144386e581f111f97
Josephine Pfeiffer <hi@josie.lol>
2023-06-02 08:56:32 +0200

    add 4.13 support

--- setup.sh
+++ setup.sh
@@ -38,8 +38,9 @@ systemctl enable --now libvirtd
 # map crc release to openshift version 
 ## from: https://github.com/crc-org/crc/releases
 declare -A VERSION_MAP=(
-  ["latest"]="2.18.0"
-  ["4.12.13"]="2.18.0" # latest
+  ["latest"]="2.20.0"
+  ["4.13.0"]="2.20.0" # latest
+  ["4.12.13"]="2.18.0"
   ["4.12.0"]="2.13.1"
   ["4.11.0"]="2.7.1"
   ["4.10.3"]="2.0.1"
@@ -48,11 +49,8 @@ declare -A VERSION_MAP=(
 # look up the CRC release version
 CRC_VERSION=${VERSION_MAP[$VERSION]}
 
-# download release
-curl -L -O "https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/crc/${CRC_VERSION}/crc-linux-amd64.tar.xz"
-
-# Check if the download was successful
-if [[ $? -eq 0 ]]
+# download release & check if the download was successful
+if ! curl -L -O "https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/crc/${CRC_VERSION}/crc-linux-amd64.tar.xz";
 then
     echo "Download complete."
 else
@@ -65,7 +63,7 @@ tar -xvf crc-linux-amd64.tar.xz
 mkdir -p /usr/local/bin
 cp crc-linux-*-amd64/crc /usr/local/bin/
 export PATH=$PATH:/usr/local/bin
-echo 'export PATH=$PATH:root/bin' >> /root/.bashrc
+echo "export PATH=$PATH:root/bin" >> /root/.bashrc
 
 rm -rf crc-linux-*-amd64
 rm crc-linux-amd64.tar.xz
